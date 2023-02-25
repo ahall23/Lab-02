d327363c79b77b1e6ff010d503a2b107a7e574ba
Alexia Hall <105994492+ahall23@users.noreply.github.com>
2023-02-25 16:55:30 -0600

    Update guess.js

--- 201 Labs /guess.js
+++ 201 Labs /guess.js
@@ -1,5 +1,12 @@
-let guessNo = "2";
-let body = document.querySelector("body");
-while(true){
-    let num = Number
+function aboutMe() {
+  let name = prompt("What is your name?");
+  let age = prompt("how old are you?");
+
+  if (age > 18) {
+    alert("😎 Thats great!" + " " + name + " " + "You can get some fenugreek!");
+  } else {
+    alert("You so precious" + " " + name);
+  }
 }
+
+let aboutMe = document.querySelector("button");
